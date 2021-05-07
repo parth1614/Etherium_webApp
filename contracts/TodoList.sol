2c8a8280697664447bcdbb1f81fd973e6e52b103
Parth Arora <60436959+parth1614@users.noreply.github.com>
2021-05-07 13:18:49 +0530

    Update TodoList.sol

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -6,4 +6,55 @@ contract TodoList {
 //It states the current state of smart contract
 uint public taskCount = 0;
 
+struct Task{
+
+uint id;
+
+string content;
+bool completed;
+
+
+}
+
+mapping(uint => Task) public tasks;
+
+event TaskCreated(
+
+uint id,
+string content,
+bool completed
+
+);
+
+event TaskCompleted(
+
+uint id,
+bool completed
+
+);
+
+constructor() public{
+
+createTask("Check Out The Allegory Club");
+
+}
+
+function createTask(string memory _content) public{
+
+taskCount ++;
+tasks[taskCount] = Task(taskCount, _content, false);
+
+emit TaskCreated(taskCount, _content, false);
+
+}
+
+function toggleCompleted(uint _id) public{
+
+Task memory _task = tasks[_id];
+_task.completed = !_task.completed;
+tasks[_id] = _task;
+emit TaskCompleted(_id, _task.completed);
+
+}
+
 }
